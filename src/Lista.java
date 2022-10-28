e615d873ef0cf3e48d8cddf61ff659fed4f9f94e
AlejandroR1977 <111005527+AlejandroR1977@users.noreply.github.com>
2022-10-28 10:59:42 -0700

    V3

--- src/Lista.java
+++ src/Lista.java
@@ -19,9 +19,50 @@ public class Lista {
     public void mostrarLista(){
         Nodo actual=inicio;
         while(actual!=null){
-            System.out.println(actual.dato);
+            System.out.println(actual.dato + "-->");
             actual = actual.siguiente;
         }
                 
+    }
+    //Metodo para saber si la lista esta vacia
+    public boolean listaVacia(){
+    if(inicio==null){
+        return true;
+       }
+    else{
+        return false;
+       }
+   }
+    //Metodo para insertar al final de la lista
+    public void insertarFinal(int dato){
+        if(listaVacia()){
+            inicio = new Nodo(dato);
+            fin = inicio;
+        }
+        else{
+            fin.siguiente = new Nodo(dato);
+            fin = fin.siguiente;
+        }
+    }
+    //Metodo para buscar un elemento
+    public boolean buscarEnLista(int elemento){
+        Nodo actual = inicio;
+        while(actual!= null && actual.dato!=elemento){
+            actual = actual.siguiente;
+        }
+        return actual!=null;
+    }
+    
+    //Eliminar al inicio
+    public int elimininarInicio(){
+     int elemento = inicio.dato;
+     if(inicio==fin){ //Nomas hay 1 elemento
+         inicio = null;
+         fin = null;
+     }
+     else{
+      inicio = inicio.siguiente;   
+     }         
+        return elemento;   
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,51 @@ public class Main {
         Lista lista = new Lista();
         
         do{
+            try{
+                
+            
            opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "1. Insertar un elemento al inicio\n" + "2. Insertar un elemento al final\n" + 
                    "3. Insertar un elemento en medio\n" + "1. Eliminar un elemento al inicio\n" + "2. Eliminar un elemento al final\n"+ 
                    "3. Eliminar un elemento en medio\n" + "7. Buscar un elemento\n" + "8. Mostrar los datos en la lista\n" + "9. Salir\n", 3)); 
             
            switch(opcion){
                case 1: //Insertar un elemento al inicio 
-                   elemento = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el elemento;", "Insertado al inicio", 3));
-                   lista.insertarInicio(elemento);
+                   try{
+                     elemento = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el elemento: ", "Insertado al inicio", 3));
+                     lista.insertarInicio(elemento);  
+                   }catch (NumberFormatException n){
+                    JOptionPane.showInputDialog(null, "Error: " + n.getMessage(), "Error de ingreso",JOptionPane.ERROR_MESSAGE);
+                   }
                    break;
-               case 2: break;
+               case 2: 
+                   try{
+                     elemento = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el elemento: ", "Insertado al final", 3));
+                     lista.insertarFinal(elemento);  
+                   }catch (NumberFormatException n){
+                    JOptionPane.showInputDialog(null, "Error: " + n.getMessage(), "Error de ingreso",JOptionPane.ERROR_MESSAGE);
+                   }
+                   break;
                case 3: break;
-               case 4: break;
+               case 4: 
+                   elemento = lista.elimininarInicio();
+                   JOptionPane.showMessageDialog(null, "Se eliminio el elemento " + elemento, "Eliminar elemento al inicio ", 1);
+                   break;
                case 5: break;
                case 6: break;
-               case 7: break;
+               case 7:
+                   try{
+                     elemento = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el elemento a buscar: ", "Buscar elemento", 3));
+                     lista.buscarEnLista(elemento);  
+                     if(lista.buscarEnLista(elemento)){
+                         JOptionPane.showMessageDialog(null, elemento + " encontrado en la lista", "Elemento encontrado", 3);
+                     }
+                     else{
+                         JOptionPane.showMessageDialog(null, elemento + " NO encontrado en la lista", "Elemento NO encontrado", 0);
+                     }
+                   }catch (NumberFormatException n){
+                    JOptionPane.showInputDialog(null, "Error: " + n.getMessage(), "Error de ingreso",JOptionPane.ERROR_MESSAGE);
+                   }
+                   break;
                case 8: //Mostrar los datos de la lista
                    lista.mostrarLista();
                    break;
@@ -33,9 +63,10 @@ public class Main {
                default:
                    JOptionPane.showMessageDialog(null, "Opcion Incorrecta");
                    break;
-               
-           }
-                    
+               }
+            }catch(Exception e){
+                JOptionPane.showInputDialog(null, "Error: " + e.getMessage(), "Error de opcion",JOptionPane.ERROR_MESSAGE);
+            }          
         }while(opcion!=9);
     }
 }
