334e5e2c262d9459bed1070957bf2d7e96545495
AlejandroR1977 <111005527+AlejandroR1977@users.noreply.github.com>
2022-11-14 12:08:04 -0700

    Roles

--- src/Main.java
+++ src/Main.java
@@ -13,16 +13,16 @@ public class Main {
         /*
         Pueden usar JOptionPane o hacer la interfaz por consola.
         Deben definir un menú con las siguientes opciones:
-                "1. Insertar un elemento al inicio\n"+ //TODO @Angel
+                "1. Insertar un elemento al inicio\n"+ //TODO @Walter
                 "2. Insertar un elemento al final\n"+  //TODO @Angel
                 "3. Insertar un elemento en orden\n"+  //TODO @Angel
                 "4. Eliminar un elemento al inicio\n"+ //TODO @Angelica
                 "5. Eliminar un elemento al final\n"+  //TODO @Angelica
                 "6. Eliminar un elemento\n"+           //TODO @Armando
                 "7. Buscar un elemento\n"+             //TODO @Armando
-                "8. Mostrar los datos de inicio a fin\n"+ //TODO Walter
+                "8. Mostrar los datos de inicio a fin\n"+ 
                 "9. Mostrar los datos de fin a inicio\n"+ //TODO Walter
-                "10. Salir\n","Menú de opciones",3));  //TODO Walter
+                "10. Salir\n","Menú de opciones",3));  
         */
     }
 }
