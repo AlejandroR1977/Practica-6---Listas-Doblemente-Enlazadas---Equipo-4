5c3383f0780952e68e6962ba695f7c6de500a664
AlejandroR1977 <111005527+AlejandroR1977@users.noreply.github.com>
2022-11-14 10:11:12 -0700

    TDAListaDoble

--- src/Lista.java
+++ src/Lista.java
@@ -1,68 +1,72 @@
+
 public class Lista {
-    
+
     protected Nodo inicio, fin; //Punteros para saber donde esta el inicio
-    
+
     //constructor para crear una lista vacia
-    public Lista(){
+    public Lista() {
         inicio = null;
         fin = null;
     }
-    
+
+    //Metodo para saber si la lista esta vacia
+    public boolean listaVacia() {
+        if (inicio == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     //metodo para agregar un Nodo al Inicio de la Lista
-    public void insertarInicio(int dato){
+    public void insertarInicio(int dato) {
         //crear el nodo
         inicio = new Nodo(dato, inicio);
-        if(fin==null){ //Si la lista esta vacia mover el apintador al fin al nodo
+        if (fin == null) { //Si la lista esta vacia mover el apintador al fin al nodo
             fin = inicio;
         }
     }
-    public void mostrarLista(){
-        Nodo actual=inicio;
-        while(actual!=null){
-            System.out.println(actual.dato + "-->");
-            actual = actual.siguiente;
-        }
-                
-    }
-    //Metodo para saber si la lista esta vacia
-    public boolean listaVacia(){
-    if(inicio==null){
-        return true;
-       }
-    else{
-        return false;
-       }
-   }
+
     //Metodo para insertar al final de la lista
-    public void insertarFinal(int dato){
-        if(listaVacia()){
+    public void insertarFinal(int dato) {
+        if (listaVacia()) {
             inicio = new Nodo(dato);
             fin = inicio;
-        }
-        else{
+        } else {
             fin.siguiente = new Nodo(dato);
             fin = fin.siguiente;
         }
     }
+
+    //Eliminar al inicio
+    public int elimininarInicio() {
+        int elemento = inicio.dato;
+        if (inicio == fin) { //Nomas hay 1 elemento
+            inicio = null;
+            fin = null;
+        } else {
+            inicio = inicio.siguiente;
+        }
+        return elemento;
+    }
+
     //Metodo para buscar un elemento
-    public boolean buscarEnLista(int elemento){
+    public boolean buscarEnLista(int elemento) {
         Nodo actual = inicio;
-        while(actual!= null && actual.dato!=elemento){
+        while (actual != null && actual.dato != elemento) {
             actual = actual.siguiente;
         }
-        return actual!=null;
+        return actual != null;
     }
-    
-    //Eliminar al inicio
-    public int elimininarInicio(){
-     int elemento = inicio.dato;
-     if(inicio==fin){ //Nomas hay 1 elemento
-         inicio = null;
-         fin = null;
-     }
-     else{
-      inicio = inicio.siguiente;   
-     }         
-        return elemento;   
+
+    public void mostrarLista() {
+        Nodo actual = inicio;
+        while (actual != null) {
+            System.out.println(actual.dato + "-->");
+            actual = actual.siguiente;
+        }
+
     }
+
 }
+
