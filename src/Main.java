d89a20ae63459bc4e0be0493d0182960a1115304
AlejandroR1977 <111005527+AlejandroR1977@users.noreply.github.com>
2022-11-14 19:12:01 -0700

    ImplementacionInterfaz V2

--- src/Main.java
+++ src/Main.java
@@ -13,17 +13,17 @@ public class Main {
         /*
         Pueden usar JOptionPane o hacer la interfaz por consola.
         Deben definir un menú con las siguientes opciones:
-                "1. Insertar un elemento al inicio\n"+ 
-                "2. Insertar un elemento al final\n"+  
-                "3. Insertar un elemento en orden\n"+  
-                "4. Eliminar un elemento al inicio\n"+
-                "5. Eliminar un elemento al final\n"+ 
-                "6. Eliminar un elemento\n"+           
-                "7. Buscar un elemento\n"+             
-                "8. Mostrar los datos de inicio a fin\n"+ 
-                "9. Mostrar los datos de fin a inicio\n"+ 
-                "10. Salir\n","Menú de opciones",3));  
-                 */
+                "1. Insertar un elemento al inicio\n"+ //TODO @Walter
+                "2. Insertar un elemento al final\n"+  //TODO @Angel
+                "3. Insertar un elemento en orden\n"+  //TODO @Angel
+                "4. Eliminar un elemento al inicio\n"+ //TODO @Angelica
+                "5. Eliminar un elemento al final\n"+  //TODO @Angelica
+                "6. Eliminar un elemento\n"+           //TODO @Armando
+                "7. Buscar un elemento\n"+             //TODO @Armando
+                "8. Mostrar los datos de inicio a fin\n"+
+                "9. Mostrar los datos de fin a inicio\n"+ //TODO Walter
+                "10. Salir\n","Menú de opciones",3)); 
+                */
         int opcion=0;
         int elemento;
         ListaDoble lista = new ListaDoble();
